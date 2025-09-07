215f72c7b73cb0352ca7c45bb09441b07aa71a0a
jestin16 <jestinshaji777@gmail.com>
2025-09-07 09:10:42 +0530

    reverse of an integer

--- reverseint.c
+++ reverseint.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 void main(){
-    int a,reversed=0,d;
+    int a,reversed=0,d,x;
     printf("\n enter a number");
     scanf("%d",&a);
+    x=a;
     while(a>0){
         d=a%10;
         reversed=(reversed*10) + d;
         a=a/10;
     }
-    printf("the number in reversed format is  %d",reversed);
+    printf("\n the number before reversed %d",x);
+    printf("\n the number in reversed format is  %d",reversed);
 }
