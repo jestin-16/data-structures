59857918470517a43ad7c6e4c58012b3bb48e8a5
jestin16 <jestinshaji777@gmail.com>
2025-12-13 23:09:18 +0530

    linear search

---
+++ linearserch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+    int a[10],n,i,flag=0,x;
+    printf("Enter the number of elements in the array:");
+    scanf("%d",&n);
+    printf("Enter %d elements:\n",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]); 
+    for(i=0;i<n;i++){
+        ifa[i]==x){
+            flag=1;
+            break;
+        }
+    }
+    if(!flag)
+    printf("Element not found in the array");
+else 
+    printf("Element found at position %d",i+1);
+    
+}
