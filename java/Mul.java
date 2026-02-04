de3a889559a93adcd0fdfc8218b3427ba9fb37c9
jestin16 <jestinshaji777@gmail.com>
2026-02-04 20:44:18 +0530

    multiplication

---
+++ java/Mul.java
@@ -0,0 +1,8 @@
+class Mul{
+    public static void main(String[] args){
+        int a=5;
+        int b=10;
+        int mul=a*b;
+        System.out.println("the multiplication is : "+mul);
+    }
+}
