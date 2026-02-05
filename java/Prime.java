c8b4464a790c298cd0923d644b8bb2c00db579a7
jestin16 <jestinshaji777@gmail.com>
2026-02-05 22:54:37 +0530

    prime

---
+++ java/Prime.java
@@ -0,0 +1,11 @@
+class Prime{
+    public static void main(String[] args) {
+        int a=10;
+        for(int i=2;i<a;i++){
+            if(a%i==0){
+                System.out.println("Not a prime number");
+                return;
+            }
+        }        System.out.println("Prime number");
+    }
+}
