989aae50aa1d40d1a2654390288e2921ad8c6994
jestin16 <jestinshaji777@gmail.com>
2026-03-23 21:06:09 +0530

    array traversal

---
+++ java/ArrayTraversal.java
@@ -0,0 +1,8 @@
+public class ArrayTraversal {
+    public static void main(String[] args) {
+        int[] array={1,3,4,5,};
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+    }
+}
+}
